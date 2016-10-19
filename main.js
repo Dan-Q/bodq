1fd4d7882c1b5f0a35ff869e5df61a43af1aac73
Dan Q <dan@danq.me>
2016-10-19 09:04:28 +0100

    Initial framework

---
+++ main.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyAUNM0PTTEnssJMYf7Y2bN_cgKy4oHw5bA",
+  authDomain: "bodq-95961.firebaseapp.com",
+  databaseURL: "https://bodq-95961.firebaseio.com",
+  storageBucket: "bodq-95961.appspot.com",
+  messagingSenderId: "112908799870",
+});
+
+$(function(){
+
+});
